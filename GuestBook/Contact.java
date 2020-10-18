b21ef95ef9d92e46739b7f300d3c28aebbd2bfc8
Jungho781 <72117787+Jungho781@users.noreply.github.com>
2020-10-17 17:17:04 -0700

    Update Contact.java

--- GuestBook/Contact.java
+++ GuestBook/Contact.java
@@ -1,11 +1,27 @@
 package GuestBook;
 
 public class Contact implements Comparable<Contact>{
+    private String firstName,lastName,address,phoneNumber;
+
+    public Contact(String firstName,String lastName,String address,String phoneNumber){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.phoneNumber=phoneNumber;
+    }
 
     @Override
     public int compareTo(Contact o) {
-        // TODO Auto-generated method stub
-        return 0;
+        int result=this.firstName.compareTo(o.firstName);
+        if(result==0){
+            result=this.lastName.compareTo(o.lastName);
+        }
+        if(result==0){
+            result=this.address.compareTo(o.address);
+        }
+        if(result==0){
+            result=this.phoneNumber.compareTo(o.phoneNumber);
+        }
+        return result;
     }
-
 }
